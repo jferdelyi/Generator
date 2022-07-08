53f53c1d3270b65130e7031320492db866fe6ca7
Jean-François Erdelyi <jean-francois.erdelyi@csgroup.eu>
2022-07-08 17:27:41 +0200

    Update angular

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,12 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClarityModule } from '@clr/angular';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GeneratorComponent } from './generator/generator.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,11 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
     GeneratorComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ClarityModule,
-    BrowserAnimationsModule,
-    HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ui/src/app/generator/generator.component.ts
+++ ui/src/app/generator/generator.component.ts
@@ -1,68 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { GeneratorService } from '../generator.service';
 
 @Component({
   selector: 'app-generator',
   templateUrl: './generator.component.html',
-  styleUrls: ['./generator.component.scss']
+  styleUrls: ['./generator.component.css']
 })
 export class GeneratorComponent implements OnInit {
 
-  /**
-   * Constructor
-   * @param generatorService generator service injection
-   */
-  public constructor(private generatorService: GeneratorService) {
-    // Nothing
+  constructor() { }
+
+  ngOnInit(): void {
   }
 
-  /**
-   * Init
-   */
-  public ngOnInit(): void {
-    this.nGramGet();
-  }
-
-  /**
-   * Generate new word
-   */
-  public generate(): void {
-    this.generatorService.generate();
-  }
-
-  /**
-   * Get nGram
-   */
-  public nGramGet() {
-    this.generatorService.nGramGet();
-  }
-
-  /**
-   * Set nGram
-   * @param nGram new nGram
-   */
-  public nGramUpdate(nGram: number): void {
-    this.generatorService.nGramUpdate(nGram);
-  }
-
-  /**
-   * Get nGram data from generator service
-   */
-  public getNGram(): number {
-    return this.generatorService.getNGram;
-  }
-
-  /**
-   * Get connection status
-   */
-  public getStatus(): boolean {
-    return this.generatorService.getStatus;
-  }
-
-  /**
-   * Get words data from generator service
-   */
-  public get getOutput(): Array<[string, string]> {
-    return this.generatorService.getOutput;
-  }
 }
